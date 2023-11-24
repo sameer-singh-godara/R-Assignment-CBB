b9642d22903678c8750cbc40bf48d3c97f14eb97
sameer-singh-godara <sameer22439@iiitd.ac.in>
2023-11-25 00:44:58 +0530

    1st done

--- Sameer_Singh_Godara;2022439;TakeHome_Assignment.R
+++ Sameer_Singh_Godara;2022439;TakeHome_Assignment.R
@@ -4,9 +4,7 @@ library(ggplot2)
 library(reshape2)
 library(tidyverse)
 
-
-location <- "~/Documents/GitHub/R-Assignment-CBB/wdbc.data"
-data <- read.csv(location, header = FALSE, sep = ",")
+data <- read.csv("~/Documents/GitHub/R-Assignment-CBB/wdbc.data", header = FALSE, sep = ",")
 
 # names of column
 name_of_column <- c("ID", "Diagnosis", "Radius_Mean", "Texture_Mean", "Perimeter_Mean", "Area_Mean", "Smoothness_Mean", "Compactness_Mean", "Concavity_Mean", "Concave_Points_Mean", "Symmetry_Mean", "Fractal_Dimension_Mean", "Radius_SE", "Texture_SE", "Perimeter_SE", "Area_SE", "Smoothness_SE", "Compactness_SE", "Concavity_SE", "Concave_Points_SE", "Symmetry_SE", "Fractal_Dimension_SE", "Radius_Worst", "Texture_Worst", "Perimeter_Worst", "Area_Worst", "Smoothness_Worst", "Compactness_Worst", "Concavity_Worst", "Concave_Points_Worst", "Symmetry_Worst", "Fractal_Dimension_Worst")
@@ -21,11 +19,8 @@ for(i in seq_along(data$Diagnosis)){
   }
 }
 
-
 #Linear Regression
-attach(data)
-model <- lm(Diagnosis ~ ., data = data[, c(3:32)])
-summary(model)
-
+model_linear <- lm(data$Diagnosis~., data = data[,c(3:32)])
+summary(model_linear)
 
 
